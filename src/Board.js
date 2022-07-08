4141aab2492cf1d5e44b5ba5ca3530b8d274f3f4
yoojj1990 <yoojj1990@gmau>
2022-07-08 13:27:23 +0900

    새로운 글 추가 기능

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Tr from './Tr';
+import Post from './Post';
 
 function Board() {
 
@@ -13,6 +14,31 @@ function Board() {
             .then(res => setInfo(res.data))
             .catch(err => console.log(err));
     },[]);
+
+    const nextId = useRef(11); // id초기값 선언(기존 원소의 개수가 10개이므로 11부터 시작)
+
+    const handleSave = (infoData) => {
+        // setInfo((prevInfo)=>{
+        //     return[
+        //         ...prevInfo,{
+        //             id:nextId.current,
+        //             name:infoData.name,
+        //             email:infoData.email,
+        //             phone:infoData.phone,
+        //             website:infoData.website
+        //         }]
+        // })
+
+        setInfo(info => info.concat({
+            id:nextId.current,
+            name:infoData.name,
+            email:infoData.email,
+            phone:infoData.phone,
+            website:infoData.website
+        }))
+
+        nextId.current += 1;
+    };
 
     return (
         <div className=''>
@@ -33,6 +59,9 @@ function Board() {
                 </thead>
                 <Tr info={info}></Tr>
             </table>
+            <br />
+            <br />
+            <Post onSaveData={handleSave}></Post>
         </div>
     );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,58 @@
 import React from 'react';
+import { useState } from "react";
 
-function Post() {
+function Post({onSaveData}) {
+
+    const [form, setForm] = useState({
+        name:'',
+        email:'',
+        phone:'',
+        website:''
+    })
+
+    const handleChange = (e) =>{
+        const {name, value} = e.target;
+        setForm({
+            ...form,
+            [name]:value
+        })
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSaveData(form)
+        // 입력폼 초기화
+        setForm({
+            name:'',
+            email:'',
+            phone:'',
+            website:''
+        })
+    };
+
     return (
-
+        <div>
+            <div className='text-center font-bold text-[25px]'>
+                고객 정보 추가하기
+            </div>
+            <form className='text-center' onSubmit={handleSubmit}>
+                <div>
+                    <label  className=''>고객 이름</label>
+                    <input className='' placeholder='이름을 입력해주세요' type="text" name="name" value={form.name} onChange={handleChange}></input>
+                    <label  className=''>이메일</label>
+                    <input className='' placeholder='이메일을 입력해주세요' type="email" name="email" value={form.email} onChange={handleChange}></input>
+                </div>
+                <div className='text-center'>
+                    <label  className=''>전화번호</label>
+                    <input className='' placeholder='전화번호를 입력해주세요' type="text" name="phone" value={form.phone} onChange={handleChange}></input>
+                    <label  className=''>홈페이지</label>
+                    <input className='' placeholder='홈페이지를 입력해주세요' type="text" name="website" value={form.website} onChange={handleChange}></input>
+                </div>
+                <div className='text-center'>
+                    <button className='' type="submit">저장</button>
+                </div>
+            </form>
+        </div>
     );
 }
 
